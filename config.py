24dc895474f1426bc5c438e811ff7748f96d057a
Peter Hu <yesky93@sina.com>
2017-04-30 14:46:54 +0800

    [TODO] grab from post url list

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import json
 config = {
             "host": "winniedezhu.blog.163.com", 
             "indexUrl":"http://winniedezhu.blog.163.com/blog/#m=0", 
-            "referUrl":"http://winniedezhu.blog.163.com/blog"
+            "referUrl":"http://winniedezhu.blog.163.com/blog",
+            "archiveAPIUrl": "http://api.blog.163.com/winniedezhu/dwr/call/plaincall/BlogBeanNew.getBlogsByArchive.dwr",
+            "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
          }
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#coding=utf8
 import os
 
 import requests
@@ -19,8 +20,46 @@ except ImportError:
     import simplejson as json
 from config import config
 
-def get_archive_list(ts, count):
-    None
+
+def get_archive_list(session, ts, count):
+    # callCount=1
+    # scriptSessionId=${scriptSessionId}187
+    # c0-scriptName=BlogBeanNew
+    # c0-methodName=getBlogsByArchive
+    # c0-id=0
+    # c0-param0=number:32617477
+    # c0-param1=number:1280592000000
+    # c0-param2=number:0
+    # c0-param3=number:14
+    # batchId=522470
+
+    payload = [
+        'callCount=1',
+        'scriptSessionId=${scriptSessionId}187',
+        'c0-scriptName=BlogBeanNew',
+        'c0-methodName=getBlogsByArchive',
+        'c0-id=0',
+        'c0-param0=number:32617477',
+        'c0-param1=number:%s' % ts ,
+        'c0-param2=number:0',
+        'c0-param3=number:%d' % count,
+        'batchId=522470'
+    ]
+
+    headers = {
+        'content-type': 'text/plain',
+        'Origin': 'http://api.blog.163.com',
+        'Refer': 'http://api.blog.163.com/crossdomain.html',
+        'Host': 'api.blog.163.com',
+        'User-Agent': config['userAgent'],
+        'Connection': 'Keep-Alive',
+        'Accept': "*/*",
+        'Accept-Encoding': "gzip, deflate"
+    }
+
+    print "\n".join(payload)
+    resp = session.post(config['archiveAPIUrl'], "\n".join(payload), headers=headers)
+    print resp.text
 
 def sync():
     cookieFile = 'cookie'
@@ -29,7 +68,7 @@ def sync():
         'Referer': config['referUrl'],
         'Accept-Language': 'zh-CN',
         'Host': config['host'],
-        'User-Agent': 'Mozilla/5.0 (compatible; MISE 9.0; Windows NT 6.1); Trident/5.0',
+        'User-Agent': config['userAgent'],
         'Connection': 'Keep-Alive'
     }
 
@@ -44,8 +83,30 @@ def sync():
     ctx = execjs.compile(data_js + "\nvar get = function(){return this.p}")
     js_data = ctx.call('get');
 
-    for item in js_data['a']:
+    for item in js_data['a'][0:2]:
         print item['archDate'], ',', item['count']
+        get_archive_list(session, item['archDate'], item['count'])
+
+def sync_post():
+    cookieFile = 'cookie'
+    cookiejar = cookielib.MozillaCookieJar(cookieFile)
+    headers = {
+        'Referer': config['referUrl'],
+        'Accept-Language': 'zh-CN',
+        'Host': config['host'],
+        'User-Agent': config['userAgent'],
+        'Connection': 'Keep-Alive'
+    }
+
+    session = requests.Session()
+    postUrl = "http://winniedezhu.blog.163.com/blog/static/32617477201072583247570/"
+    resp = session.get(postUrl, headers=headers, cookies=cookiejar)
+    result = resp.text
+
+    pageHtml = BS(result)
+    #print result
+    with open('temp.html', 'w') as temp:
+        temp.write(result.encode('gbk'))
 
 if __name__ == "__main__":
-    sync()
+    sync_post()
